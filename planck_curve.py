7c7c63ed0ee72680bceb344f5edae26d99198394
Alexa Villaume <avillaum@ucsc.edu>
2016-01-26 11:47:10 -0800

    Need to get lambda stuff working.

--- planck_curve.py
+++ planck_curve.py
@@ -1,9 +1,6 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
-from astropy import constants as const
-from astropy import units as u
-from astropy.analytic_functions import blackbody_lambda, blackbody_nu
 
 def compute_planck(freq, temp):
     """
@@ -19,11 +16,25 @@ def compute_planck(freq, temp):
 
     return brightness
 
-def plot_curve(freq, flux):
-    plt.plot(freq, flux, color='k', lw=2)
+def compute_planck_tderive(freq, temp):
+    """
+    Compute the curve of the derivative of the Planck curve
+    with respect to temperature.
+
+    Expression for this taken from Rybicki & Lightman eq. 1.55
+    """
+    exp1 = (2*(planck_c**2)*(freq**4))/((light_speed**2)*(boltzmann_c)*(temp**2))
+    exp2 = np.exp(planck_c*freq/(boltzmann_c*temp))
+
+    return exp1*(exp2/(exp2-1)**2)
+
+def plot_curve(freq, flux, peak, temp):
+    plt.plot(freq, flux, color='k', lw=2, label='Temp = {}'.format(temp))
+    plt.axvline(peak['peak'], ls='--', label='x = {}'.format(peak['x']))
     #plt.gca().invert_xaxis()
     plt.xscale('log')
     #plt.yscale('log')
+    plt.legend(frameon=False, loc='upper left')
     plt.show()
 
 def total_brightness_is(freq, flux):
@@ -34,8 +45,12 @@ def total_brightness_is(freq, flux):
     """
     return np.trapz(flux[::-1], freq[::-1])
 
-def peak_ofcurve_is(freq, flux):
+def peak_ofcurve_is(freq, flux, temp):
+    peak = np.where(flux == max(flux))
+    peak_freq = freq[peak]
 
+    const = planck_c/(boltzmann_c * temp)
+    return {'peak': peak_freq, 'x': peak_freq*const}
 
 light_speed = 2.9979e10  # cm s^-1
 planck_c = 6.626e-27     #
@@ -46,4 +61,10 @@ if __name__ == '__main__':
     flux = compute_planck(freq, temp)
 
     print total_brightness_is(freq, flux)
-    plot_curve(freq, flux)
+    peak = peak_ofcurve_is(freq, flux, temp)
+    plot_curve(freq, flux, peak, temp)
+
+    dBdT = compute_planck_tderive(freq, temp)
+    i = np.where(~np.isnan(dBdT))
+    peak = peak_ofcurve_is(freq[i], dBdT[i], temp)
+    plot_curve(freq, dBdT, peak, temp)
